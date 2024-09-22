c2dcc1ca712a8c2aa1e2386cb96b95b730cc5574
randomSmarts <67401115+randomSmarts@users.noreply.github.com>
2024-09-22 10:16:39 -0700

    sidenavs are done

--- app/ui/teacherDashboard/sidenav.tsx
+++ app/ui/teacherDashboard/sidenav.tsx
@@ -27,6 +27,12 @@ export default function SideNav() {
             {/* Navigation Links */}
             <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
                 <nav className="flex flex-col space-y-2 w-full">
+                    {/* Home Link */}
+                    <Link legacyBehavior={true} href="/app/student/dashboard" passHref>
+                        <a className="flex items-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600">
+                            <span>Home</span>
+                        </a>
+                    </Link>
 
                     {/* School Classes Link with Dropdown */}
                     <div className="flex flex-col">
