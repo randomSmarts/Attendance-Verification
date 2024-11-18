d193d5f6fbe1dfd75096d249ed5ffc53479ae297
randomSmarts <67401115+randomSmarts@users.noreply.github.com>
2024-11-17 22:23:11 -0800

    Reverted testing with setting type ids
    TODO:
    - figure out why it just randomly deleted it where im not creating an account anymore
    - when i do the attendance make sure it checks that im not a teacher, and then if i fail it sets attendance to false
    - able to generate attendance reports
    - fix all the errors in the different files
    - have teacher able to set custom time periods for when the student can sign in
    - better ui for creating classes
    - set custom latitude and longitude for each class location

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 
-
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     env: {
         POSTGRES_URL: process.env.POSTGRES_URL,
     },
-}
+    output: 'export', // Ensures static files for GitHub Pages
+    basePath: '/Attendance-Verification', // Replace <repository-name> with your repo name
+    trailingSlash: true, // Adds a trailing slash to all routes
+    typescript: {
+        ignoreBuildErrors: true, // Disables TypeScript build errors
+    },
+};
+
 export default nextConfig;
